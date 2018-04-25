f80672a619966df8b01bcef65c0b522e3692594b
张昭阳 <296753977@qq.com>
2018-04-25 22:10:41 +0800

    version 0.0.2 add mergeSort and heapSort in Sort.java

--- Algorithm/src/Sort/Sort.java
+++ Algorithm/src/Sort/Sort.java
@@ -24,6 +24,7 @@ public class Sort {
 	}
 
 	/**
+	 * 快速排序
 	 * 
 	 * @param 排序前数组a
 	 * @param 待排序数组左边界left
@@ -62,46 +63,105 @@ public class Sort {
 	 * 
 	 * @param num
 	 */
-	public void pileSort(int[] arr) {
-		// 初始化堆，堆是个完全二叉树。从最后一个非叶子节点开始调整成堆，保证后面的筛选算法正确性
+	// 初始化堆，堆是个完全二叉树。从最后一个非叶子节点开始调整成堆，保证后面的筛选算法正确性
+	private int[] buildMaxHeap(int[] array) {
+		// 自下而上构造（从最后一个非叶子节点开始调整成堆）
+		for (int i = (array.length - 1) / 2 - 1; i >= 0; i--) {
+			adjustDownToUp(array, i, array.length);
+		}
+		return array;
+	}
 
-		// 如果是升序则使用最大堆，如果是降序则使用最小堆，每形成一次最大或最小堆，就将无序区的第一个元素和最后一个元素换位置
+	// 将元素array[k]自下向上逐渐调整树形结构,k假如为父节点、2k+1为左孩子，2k+2为右孩子
+	// k的子树都是按照大根堆排好序的
+	private void adjustDownToUp(int[] array, int k, int length) {
+		int temp = array[k];
+		// 从第k个节点的左子节点开始
+		for (int i = 2 * k + 1; i < length - 1; i = 2 * i + 1) {
+			// 如果左孩子小于右孩子，则当前最大的是右孩子
+			if (array[i] < array[i + 1]) {
+				i++;
+			}
+			// 如果父节点大于两个孩子节点中的最大值，由于初始堆调整为大根堆了，在它以下的子树都符合大根堆特征，就没必要继续循环下去了
+			if (temp >= array[i]) {
+				break;
+			} else {
+				array[k] = array[i];
+				k = i;// 修改关键字指针
+			}
+		}
+		array[k] = temp;
+	}
 
+	public int[] heapSort(int[] arr) {
 		// 每次无序区只有根元素不符合最大最小堆的性质，所以用筛选算法从根元素开始调整成最小最大堆的形式
+		arr = buildMaxHeap(arr);
+		// 如果是升序则使用最大堆，如果是降序则使用最小堆，每形成一次最大或最小堆，就将无序区的第一个元素和最后一个元素换位置
+		for (int i = arr.length - 1; i > 1; i--) {
+			int temp = arr[i];
+			arr[i] = arr[0];
+			arr[0] = temp;
+			adjustDownToUp(arr, 0, i);
+		}
+		return arr;
 	}
 
 	/**
-	 * 插值排序
-	 * 
-	 * @param array
+	 * 插值排序 插入排序算法有种递归的思想在里面，它由N-1趟排序组成。初始时，只考虑数组下标0处的元素，只有一个元素，显然是有序的。 然后第一趟
+	 * 对下标 1 处的元素进行排序，保证数组[0,1]上的元素有序； 第二趟 对下标 2 处的元素进行排序，保证数组[0,2]上的元素有序；
+	 * 第N-1趟对下标 N-1 处的元素进行排序，保证数组[0,N-1]上的元素有序，也就是整个数组有序了。 它的递归思想就体现在：当对位置 i
+	 * 处的元素进行排序时，[0,i-1]上的元素一定是已经有序的了
 	 */
-	/*
-	 * 插入排序算法有种递归的思想在里面，它由N-1趟排序组成。初始时，只考虑数组下标0处的元素，只有一个元素，显然是有序的。
-	 * 
-	 * 然后第一趟 对下标 1 处的元素进行排序，保证数组[0,1]上的元素有序；
-	 * 
-	 * 第二趟 对下标 2 处的元素进行排序，保证数组[0,2]上的元素有序；
-	 * 
-	 * .....
-	 * 
-	 * .....
-	 * 
-	 * 第N-1趟对下标 N-1 处的元素进行排序，保证数组[0,N-1]上的元素有序，也就是整个数组有序了。
-	 * 
-	 * 它的递归思想就体现在：当对位置 i 处的元素进行排序时，[0,i-1]上的元素一定是已经有序的了
-	 */public void insertSort(int[] array) {
-		 int temp;
-		 for(int p=1;p<array.length;p++)
-		 {
-			 int j = p;
-			 temp=array[p];
-			 for(;j>0&&temp<array[j-1];j--)
-			 {
-				 //后移元素
-				array[j]=array[j-1];
-			 }
-			 array[j]=temp;
-		 }
+	public void insertSort(int[] array) {
+		int temp;
+		for (int p = 1; p < array.length; p++) {
+			int j = p;
+			temp = array[p];
+			for (; j > 0 && temp < array[j - 1]; j--) {
+				// 后移元素
+				array[j] = array[j - 1];
+			}
+			array[j] = temp;
+		}
 	}
 
+	/**
+	 * 归并排序
+	 * 
+	 * @param array
+	 * @param low
+	 * @param high
+	 */
+	public void mergeSort(int[] array, int low, int high) {
+		int mid = (high + low) / 2;
+		if (low < high) {
+			mergeSort(array, low, mid);
+			mergeSort(array, mid + 1, high);
+			merge(array, low, mid, high);
+		}
+	}
+
+	private void merge(int[] array, int low, int mid, int high) {
+		// int mid = (high+low)/2;
+		int i = low;
+		int j = mid + 1;
+		int k = 0;
+		int[] temp = new int[high - low + 1];
+		while (i <= mid && j <= high) {
+			if (array[i] < array[j]) {
+				temp[k++] = array[i++];
+			} else {
+				temp[k++] = array[j++];
+			}
+		}
+		while (i <= mid) {
+			temp[k++] = array[i++];
+		}
+		while (j <= high) {
+			temp[k++] = array[j++];
+		}
+		for (int x = 0; x < temp.length; x++) {
+			array[x + low] = temp[x];
+		}
+	}
 }
